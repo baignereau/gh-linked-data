fbf74cd61f57e9cfa23d8e6c444768ecb9e81561
Alexandre Bertails <bertails@gmail.com>
2012-01-08 15:36:29 -0500

    ~ use more stable scala version

--- aRDF/project/build.scala
+++ aRDF/project/build.scala
@@ -11,16 +11,24 @@ object BuildSettings {
     scalaVersion := "2.9.1",
     scalaHome    := Some {
       val bin = file("bin")
-      def latest = (bin.listFiles.toList filter { _.getName startsWith "scala-2.10.0.r" } sortWith { _.getName > _.getName }).headOption
+      def latest =
+        (bin.listFiles.toList filter {
+          _.getName startsWith "scala-2.10.0.r" } sortWith {
+            _.getName > _.getName })
+        .headOption
       latest match {
         case Some(scala) => {
           logger.info("found nightly build of scala compiler: %s" format scala.getName)
           scala
         }
         case None => {
-          logger.info("downloading the latest scala compiler (nightly builds)")
-          val u = url("http://www.scala-lang.org/archives/downloads/distrib/files/nightly/distributions/scala-2.10.0.latest.zip")
-          val zip = file("/tmp/scala-latest.zip")
+          val scalaLang = "http://www.scala-lang.org/archives/downloads/distrib/files/nightly/distributions/"
+          val jay = "http://jay.w3.org/~bertails/public/scala/"
+          val baseURL = jay
+          val scalaVersion = "scala-2.10.0.rdev-4005-2011-12-13-g278a225"
+          val u = url("%s%s.zip" format (jay, scalaVersion))
+          val zip = file("/tmp/%s.zip" format scalaVersion)
+          logger.info("downloading %s" format u.toString)
           u #> zip !;
           Process("unzip -o %s -d %s" format (zip.getAbsolutePath, bin.getAbsolutePath)) !;
           latest getOrElse sys.error("couldn't download and set the latest scala compiler") 
